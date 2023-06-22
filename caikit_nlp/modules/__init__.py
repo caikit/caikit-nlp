a49aa8f15bbb70cd2cef2755040212dc9f4459fa
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2023-06-22 12:06:38 -0600

    :sparkles: Expose modules
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

--- caikit_nlp/modules/__init__.py
+++ caikit_nlp/modules/__init__.py
@@ -12,4 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Local
-from . import text_generation
+from . import (
+    sentence_classification,
+    sentence_split,
+    sequence_classification,
+    text_generation,
+)
