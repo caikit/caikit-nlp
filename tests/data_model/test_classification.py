c63746742f8f186f5bc8a40ae211ff82fb7ff5b5
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2023-06-26 16:12:34 -0600

    :white_check_mark: Additional classification tests
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

--- tests/data_model/test_classification.py
+++ tests/data_model/test_classification.py
@@ -17,12 +17,22 @@ from caikit_nlp import data_model as dm
 
 ## Setup #########################################################################
 
-dummy_classification1 = dm.Classification(label="temperature", score=0.71)
+classification1 = dm.Classification(label="temperature", score=0.71)
 
-dummy_classification2 = dm.Classification(label="conditions", score=0.98)
+classification2 = dm.Classification(label="conditions", score=0.98)
 
-dummy_classification_result = dm.ClassificationResult(
-    results=[dummy_classification1, dummy_classification2]
+classification_result = dm.ClassificationResult(
+    results=[classification1, classification2]
+)
+
+token_classification1 = dm.TokenClassification(
+    start=0, end=5, word="moose", entity="animal", score=0.8
+)
+token_classification2 = dm.TokenClassification(
+    start=7, end=12, word="goose", entity="animal", score=0.7
+)
+token_classification_result = dm.TokenClassificationResult(
+    results=[token_classification1, token_classification2]
 )
 
 ## Tests ########################################################################
@@ -37,13 +47,13 @@ def test_classification_all_fields_accessible():
 
 
 def test_classification_from_proto_and_back():
-    new = dm.Classification.from_proto(dummy_classification1.to_proto())
+    new = dm.Classification.from_proto(classification1.to_proto())
     assert new.label == "temperature"
     assert new.score == 0.71
 
 
 def test_classification_from_json_and_back():
-    new = dm.Classification.from_json(dummy_classification1.to_json())
+    new = dm.Classification.from_json(classification1.to_json())
     assert new.label == "temperature"
     assert new.score == 0.71
 
@@ -52,13 +62,13 @@ def test_classification_from_json_and_back():
 
 
 def test_classification_result_all_fields_accessible():
-    classification_result = dm.ClassificationResult(results=[dummy_classification1])
+    classification_result = dm.ClassificationResult(results=[classification1])
     assert classification_result.results[0].label == "temperature"
     assert classification_result.results[0].score == 0.71
 
 
 def test_classification_result_from_proto_and_back():
-    new = dm.ClassificationResult.from_proto(dummy_classification_result.to_proto())
+    new = dm.ClassificationResult.from_proto(classification_result.to_proto())
     assert new.results[0].label == "temperature"
     assert new.results[0].score == 0.71
     assert new.results[1].label == "conditions"
@@ -66,8 +76,72 @@ def test_classification_result_from_proto_and_back():
 
 
 def test_classification_result_from_json_and_back():
-    new = dm.ClassificationResult.from_json(dummy_classification_result.to_json())
+    new = dm.ClassificationResult.from_json(classification_result.to_json())
     assert new.results[0].label == "temperature"
     assert new.results[0].score == 0.71
     assert new.results[1].label == "conditions"
     assert new.results[1].score == 0.98
+
+
+### TokenClassification
+
+
+def test_token_classification_all_fields_accessible():
+    token_classification = dm.TokenClassification(
+        start=0,
+        end=28,
+        word="The cow jumped over the moon",
+        entity="neutral",
+        score=0.6,
+    )
+    assert token_classification.start == 0
+    assert token_classification.end == 28
+    assert token_classification.word == "The cow jumped over the moon"
+    assert token_classification.entity == "neutral"
+    assert token_classification.score == 0.6
+
+
+def test_classification_from_proto_and_back():
+    new = dm.TokenClassification.from_proto(token_classification1.to_proto())
+    assert new.start == 0
+    assert new.word == "moose"
+    assert new.score == 0.8
+
+
+def test_classification_from_json_and_back():
+    new = dm.TokenClassification.from_json(token_classification1.to_json())
+    assert new.start == 0
+    assert new.word == "moose"
+    assert new.score == 0.8
+
+
+### TokenClassificationResult
+
+
+def test_token_classification_result_all_fields_accessible():
+    token_classification_result = dm.TokenClassificationResult(
+        results=[token_classification1]
+    )
+    assert token_classification_result.results[0].start == 0
+    assert token_classification_result.results[0].word == "moose"
+    assert token_classification_result.results[0].score == 0.8
+
+
+def test_token_classification_result_from_proto_and_back():
+    new = dm.TokenClassificationResult.from_proto(
+        token_classification_result.to_proto()
+    )
+    assert new.results[0].start == 0
+    assert new.results[0].word == "moose"
+    assert new.results[0].score == 0.8
+    assert new.results[1].end == 12
+    assert new.results[1].entity == "animal"
+
+
+def test_classification_result_from_json_and_back():
+    new = dm.TokenClassificationResult.from_json(token_classification_result.to_json())
+    assert new.results[0].start == 0
+    assert new.results[0].word == "moose"
+    assert new.results[0].score == 0.8
+    assert new.results[1].end == 12
+    assert new.results[1].entity == "animal"
