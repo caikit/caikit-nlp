628c47015420ab75d897ec54294e976c905581bc
Alex-Brooks <Alex.Brooks@ibm.com>
2023-07-21 15:18:33 -0600

    Better error handling and cleanup in tok builder
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>
    
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- caikit_nlp/toolkit/data_prep_utils.py
+++ caikit_nlp/toolkit/data_prep_utils.py
@@ -14,21 +14,72 @@
 
 """Helper functions for preparing training data, e.g., preparing data formats.
 """
+# Standard
 from copy import deepcopy
-from caikit.core.data_model import DataStream
-from ..data_model import GenerationTrainRecord
-from ..resources.pretrained_model import (
-    HFAutoCausalLM,
-    HFAutoSeq2SeqLM
-)
+from typing import Callable, Tuple
 
+# First Party
+from caikit.core.data_model import DataStream
+from caikit.core.toolkit import error_handler
+import alog
+
+# Local
+from ..data_model import GenerationTrainRecord
+from ..resources.pretrained_model import HFAutoCausalLM, HFAutoSeq2SeqLM
+
+log = alog.use_channel("DATA_PREP")
+error = error_handler.get(log)
 
 IGNORE_ID = -100
 
-def build_tokenize_function(tokenizer, max_source_length, max_target_length, verbalizer, task_type):
-    def tokenize_function_language_model(example: GenerationTrainRecord):
-        # NOTE: Truncation size for source is kind of silly - source/target seqs are disconnected.
-        # Seq2seq should have the same issues...
+
+def build_tokenize_function(
+    tokenizer: "AutoTokenizer",
+    max_source_length: int,
+    max_target_length: int,
+    verbalizer: str,
+    task_type: str,
+) -> Tuple[Callable, bool]:
+    """Builds tokenizer functions which can be mapped over train streams to process
+    data which can then be easily passed to a DataLoader.
+
+    Args:
+        tokenizer: AutoTokenizer
+            Model tokenizer to be used in preprocessing, i.e., when we iterate over our data.
+        max_source_length: int
+            Max length of sequences being considered.
+        max_target_length: int
+            Max length of target sequences being predicted.
+        verbalizer: str
+            Verbalizer template to be used for formatting data. This template may use brackets
+            to indicate where fields from the data model TrainGenerationRecord must be rendered.
+        task_type: str
+            Str indicating which task is being accomplished; currently used for determining
+            tokenization / preprocessing behavior.
+
+    Returns:
+        Tuple(Callable, bool)
+            Mappable tokenize function to be applied to a training stream and bool indicating
+            whether or not the stream needs to be unwrapped, i.e., each sample yields a stream
+            of 1+ samples.
+    """
+
+    def tokenize_function_language_model(
+        example: GenerationTrainRecord,
+    ) -> "BatchEncoding":
+        """Tokenization function to be used for causallm training; this function consumes a
+        GenerationTrainRecord object and applies the verbalizer to it followed by
+        the model tokenizer. Due to the nature of our training data with src/target seqs,
+        each sample yields one example per token in the target sequence.
+
+        Args:
+            example: GenerationTrainRecord
+                Training data model object to convert a form we can learn on.
+
+        Returns:
+            transformers.tokenization_utils_base.BatchEncoding
+                encoded tokenization output corresponding to the input example.
+        """
         source_ids = tokenizer(
             example.input, max_length=max_source_length, truncation=True
         )
@@ -94,9 +145,14 @@ def build_tokenize_function(tokenizer, max_source_length, max_target_length, ver
         model_inputs["labels"] = labels
         model_inputs["task_ids"] = 0
         return model_inputs
+
     if task_type == HFAutoCausalLM.TASK_TYPE:
         return (tokenize_function_language_model, True)
     elif task_type == HFAutoSeq2SeqLM.TASK_TYPE:
         return (tokenize_function_seq2seq, False)
-    else:
-        raise ValueError("Task unsupported")
+    error(
+        "<NLP19427812E>",
+        ValueError(
+            f"Tokenizer function building only support for Causal LM / Seq2Seq models"
+        ),
+    )
