89b2858afc452eac60e90665c4c55f9aa155a08d
Alex-Brooks <Alex.Brooks@ibm.com>
2023-07-25 17:12:59 -0500

    Add test for multiworker shuffling
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- tests/toolkit/test_data_stream_wrapper.py
+++ tests/toolkit/test_data_stream_wrapper.py
@@ -5,7 +5,6 @@ from unittest import mock
 
 # Third Party
 from torch.utils.data._utils import worker
-import pytest
 
 # First Party
 from caikit.core.data_model import DataStream
@@ -66,30 +65,35 @@ def test_shuffle_full_buffer(requires_determinism):
     assert not all(test_results)
 
 
-# def test_iter_with_multi_worker(requires_determinism):
-#     """Ensure that we are able to iterate properly over data in case of workers
-#     managed by torch"""
-
-#     test_results = []
-#     # Get the IDs of all objects in the stream
-#     get_stream_id_order = lambda s: [id(datum) for datum in s]
-#     # Compare the data stream at two different iteration points; here, we
-#     # produce True if two streams have the same objects in the same order
-#     have_same_id_order = lambda id_set1, id_set2: all(
-#         [datum1 == datum2 for datum1, datum2 in zip(id_set1, id_set2)]
-#     ) and len(id_set1) == len(id_set2)
-
-#     dummy_worker_info = worker.WorkerInfo(
-#         id=1,
-#         num_workers=2,
-#         seed=7,
-#     )
-
-#     with mock.patch.object(worker, '_worker_info', dummy_worker_info):
-#         wrapper = SimpleIterableStreamWrapper(stream=SAMPLE_STREAM, shuffle=False)
-#         initialize_order = get_stream_id_order(wrapper)
-#         for _ in range(NUM_CYCLES):
-#             cycle_ids = get_stream_id_order(wrapper)
-#             test_res = have_same_id_order(initialize_order, cycle_ids)
-#             test_results.append(test_res)
-#         assert not all(test_results)
+def test_iter_with_multi_worker():
+    """Ensure that we are able to iterate properly over data in case of workers
+    managed by torch"""
+    test_results = []
+    w1_info = worker.WorkerInfo(id=0, num_workers=3, seed=7)
+    w2_info = worker.WorkerInfo(id=1, num_workers=3, seed=7)
+    w3_info = worker.WorkerInfo(id=2, num_workers=3, seed=7)
+    # Worker distribution works round robin after we consider shuffling.
+    # Since we don't shuffle in this patched test, they should just be
+    # divided as is.
+    index_stream = [
+        {"label": 0}, # goes to worker 0
+        {"label": 1}, # goes to worker 1
+        {"label": 2}, # goes to worker 2
+        {"label": 3}, # goes to worker 0
+        {"label": 4}, # goes to worker 1
+        {"label": 5}, # goes to worker 2
+    ]
+    worker_info = [
+        (w1_info, [index_stream[0], index_stream[3]]),
+        (w2_info, [index_stream[1], index_stream[4]]),
+        (w3_info, [index_stream[2], index_stream[5]]),
+    ]
+    for (dummy_worker, expected_elements) in worker_info:
+        with mock.patch.object(worker, '_worker_info', dummy_worker):
+            wrapper = SimpleIterableStreamWrapper(stream=index_stream, shuffle=False)
+            for _ in range(NUM_CYCLES):
+                actual_elements = list(wrapper)
+                test_results.append(
+                    actual_elements == expected_elements and len(actual_elements) == len(expected_elements)
+                )
+    assert all(test_results)
