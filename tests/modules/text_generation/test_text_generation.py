eed8e7abb05f7dab982e98bed8333d49a7d8a4c9
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2023-07-24 18:09:55 -0600

    :white_check_mark: Text generation tests with mock Generate and GenerateStream
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

--- tests/modules/text_generation/test_text_generation.py
+++ tests/modules/text_generation/test_text_generation.py
@@ -1,6 +1,7 @@
 """Tests for text-generation module
 """
 # Standard
+from typing import Iterable
 from unittest import mock
 import os
 import tempfile
@@ -9,6 +10,7 @@ import tempfile
 import pytest
 
 # First Party
+from caikit.interfaces.nlp.data_model import GeneratedTextResult
 from caikit_tgis_backend import TGISBackend
 
 # Local
@@ -17,6 +19,8 @@ from tests.fixtures import CAUSAL_LM_MODEL, SEQ2SEQ_LM_MODEL
 
 ### Stub Modules
 
+SAMPLE_TEXT = "Hello stub"
+
 # Helper stubs / mocks; we use these to patch caikit so that we don't actually
 # test the TGIS backend directly, and instead stub the client and inspect the
 # args that we pass to it.
@@ -24,8 +28,23 @@ class StubClient:
     def __init__(self, base_model_name):
         pass
 
-    # Generation calls on this class are a mock that explodes when invoked
-    Generate = mock.Mock(side_effect=RuntimeError("TGIS client is a mock!"))
+    def Generate(request, context):
+        fake_response = mock.Mock()
+        fake_result = mock.Mock()
+        fake_result.stop_reason = 5
+        fake_result.generated_token_count = 1
+        fake_result.text = "moose"
+        fake_response.responses = [fake_result]
+        return fake_response
+
+    def GenerateStream(request, context):
+        fake_stream = mock.Mock()
+        fake_stream.stop_reason = 5
+        fake_stream.generated_token_count = 1
+        fake_stream.seed = 10
+        fake_stream.text = "moose"
+        for i in range(3):
+            yield fake_stream
 
 
 class StubBackend(TGISBackend):
@@ -38,9 +57,28 @@ def test_bootstrap_and_run_causallm():
 
     model = TextGeneration.bootstrap(CAUSAL_LM_MODEL, load_backend=StubBackend())
 
-    sample_text = "Hello stub"
-    with pytest.raises(RuntimeError):
-        model.run(sample_text, preserve_input_text=True)
+    result = model.run(SAMPLE_TEXT, preserve_input_text=True)
+    assert isinstance(result, GeneratedTextResult)
+    assert result.generated_text == "moose"
+    assert result.generated_tokens == 1
+    assert result.finish_reason == 5
+
+
+def test_bootstrap_and_run_stream_out_causallm():
+    """Check if we can bootstrap and run_stream_out on causallm models"""
+
+    model = TextGeneration.bootstrap(CAUSAL_LM_MODEL, load_backend=StubBackend())
+
+    stream_result = model.run_stream_out(SAMPLE_TEXT, preserve_input_text=True)
+    assert isinstance(stream_result, Iterable)
+    # Convert to list to more easily check outputs
+    result_list = list(stream_result)
+    assert len(result_list) == 3
+    first_result = result_list[0]
+    assert first_result.generated_text == "moose"
+    assert first_result.details.finish_reason == 5
+    assert first_result.details.generated_tokens == 1
+    assert first_result.details.seed == 10
 
 
 def test_bootstrap_and_run_seq2seq():
@@ -48,9 +86,53 @@ def test_bootstrap_and_run_seq2seq():
 
     model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL, load_backend=StubBackend())
 
-    sample_text = "Hello stub"
-    with pytest.raises(RuntimeError):
-        model.run(sample_text, preserve_input_text=True)
+    result = model.run(SAMPLE_TEXT, preserve_input_text=True)
+    assert isinstance(result, GeneratedTextResult)
+    assert result.generated_text == "moose"
+    assert result.generated_tokens == 1
+    assert result.finish_reason == 5
+
+
+def test_bootstrap_and_run_stream_out_seq2seq():
+    """Check if we can bootstrap and run_stream_out on seq2seq models"""
+    model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL, load_backend=StubBackend())
+
+    stream_result = model.run_stream_out(SAMPLE_TEXT)
+    assert isinstance(stream_result, Iterable)
+    # Convert to list to more easily check outputs
+    result_list = list(stream_result)
+    assert len(result_list) == 3
+    first_result = result_list[0]
+    assert first_result.generated_text == "moose"
+    assert first_result.details.finish_reason == 5
+    assert first_result.details.generated_tokens == 1
+    assert first_result.details.seed == 10
+
+
+def test_run_multi_response_errors():
+    """Check if multiple responses errors"""
+    with mock.patch.object(StubClient, "Generate") as mock_gen_stream:
+        fake_response = mock.Mock()
+        fake_response.responses = [mock.Mock(), mock.Mock()]
+        mock_gen_stream.return_value = fake_response
+
+        model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL, load_backend=StubBackend())
+        with pytest.raises(ValueError):
+            model.run(SAMPLE_TEXT, preserve_input_text=True)
+
+
+def test_run_stream_out_with_runtime_error():
+    """Check if runtime error from client raises"""
+
+    with mock.patch.object(StubClient, "GenerateStream") as mock_gen_stream:
+        mock_gen_stream.side_effect = RuntimeError("An error!")
+
+        model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL, load_backend=StubBackend())
+        with pytest.raises(RuntimeError):
+            response = model.run_stream_out(SAMPLE_TEXT, preserve_input_text=True)
+            for _ in response:
+                # Need to iterate over stream for error
+                pass
 
 
 def test_bootstrap_and_save_model():
@@ -71,6 +153,8 @@ def test_save_model_can_run():
         model.save(model_dir)
         del model
         new_model = TextGeneration.load(model_dir, load_backend=StubBackend())
-        sample_text = "Hello stub"
-        with pytest.raises(RuntimeError):
-            new_model.run(sample_text, preserve_input_text=True)
+        result = new_model.run(SAMPLE_TEXT, preserve_input_text=True)
+        assert isinstance(result, GeneratedTextResult)
+        assert result.generated_text == "moose"
+        assert result.generated_tokens == 1
+        assert result.finish_reason == 5
