5f936771fe01e326b51f0b22162c04180db7fba7
Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>
2023-09-05 08:23:37 -0700

    🐛 stop_sequences as list of str
    Signed-off-by: Thara Palanivel <130496890+tharapalanivel@users.noreply.github.com>

--- caikit_nlp/modules/text_generation/peft_tgis_remote.py
+++ caikit_nlp/modules/text_generation/peft_tgis_remote.py
@@ -181,7 +181,7 @@ class PeftPromptTuningTGIS(ModuleBase):
         exponential_decay_length_penalty: Optional[
             Union[Tuple[int, float], ExponentialDecayLengthPenalty]
         ] = None,
-        stop_sequences: Optional[str] = None,
+        stop_sequences: Optional[List[str]] = None,
     ) -> GeneratedTextResult:
         """Run inference against the model running in TGIS.
 
@@ -237,7 +237,7 @@ class PeftPromptTuningTGIS(ModuleBase):
         exponential_decay_length_penalty: Optional[
             Union[Tuple[int, float], ExponentialDecayLengthPenalty]
         ] = None,
-        stop_sequences: Optional[str] = None,
+        stop_sequences: Optional[List[str]] = None,
     ) -> Iterable[GeneratedTextStreamResult]:
         """Run output stream inferencing against the model running in TGIS
 
