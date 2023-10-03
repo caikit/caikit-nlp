c539256c3aabfde4cfc3a509e76c34454782649f
gkumbhat <kumbhat.gaurav@gmail.com>
2023-10-02 19:34:02 -0500

    :technologist: Fix docstring formatting function call
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- caikit_nlp/modules/text_generation/peft_prompt_tuning.py
+++ caikit_nlp/modules/text_generation/peft_prompt_tuning.py
@@ -172,16 +172,14 @@ class PeftPromptTuning(ModuleBase):
         ] = None,
         stop_sequences: Optional[List[str]] = None,
     ) -> GeneratedTextResult:
+        f"""
+        Run the full text generation model.
+        Args:
+            {GENERATE_FUNCTION_ARGS}
+        Returns:
+            GeneratedTextResult
+                Generated text result produced by PEFT / Transformers.
         """
-            Run the full text generation model.
-            Args:
-                {}
-            Returns:
-                GeneratedTextResult
-                    Generated text result produced by PEFT / Transformers.
-        """.format(
-            GENERATE_FUNCTION_ARGS
-        )
 
         verbalized_text = render_verbalizer(self.verbalizer, {"input": text})
 
@@ -232,7 +230,7 @@ class PeftPromptTuning(ModuleBase):
         ] = None,
         stop_sequences: Optional[List[str]] = None,
     ) -> Iterable[GeneratedTextStreamResult]:
-        """Run the text generation model with output streaming
+        f"""Run the text generation model with output streaming
 
         NOTE: This implementation is marked as WIP since the API for
         HuggingFace streamer classes at time of implementation is still
@@ -240,13 +238,11 @@ class PeftPromptTuning(ModuleBase):
         Ref. https://huggingface.co/docs/transformers/v4.30.0/generation_strategies#streaming
 
         Args:
-            {}
+            {GENERATE_FUNCTION_ARGS}
 
         Returns:
             Iterable[GeneratedTextStreamResult]
-        """.format(
-            GENERATE_FUNCTION_ARGS
-        )
+        """
 
         # Apply the verbalizer to our text string
         verbalized_text = render_verbalizer(self.verbalizer, {"input": text})
