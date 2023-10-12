959edece79b8d94a50436654728330f1df9ba729
Gaurav Kumbhat <kumbhat.gaurav@gmail.com>
2023-10-11 19:47:43 -0500

    Revert ":goal_net: Disallow empty train streams"
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- caikit_nlp/modules/text_generation/peft_prompt_tuning.py
+++ caikit_nlp/modules/text_generation/peft_prompt_tuning.py
@@ -350,9 +350,6 @@ class PeftPromptTuning(ModuleBase):
             PeftPromptTuning
                 Instance of this class with tuned prompt vectors.
         """
-        error.value_check(
-            "<NLP46653367E>", len(train_stream) > 0, "train_stream cannot be empty"
-        )
 
         # Configure random seed
         transformers.set_seed(seed)
@@ -391,10 +388,6 @@ class PeftPromptTuning(ModuleBase):
 
         train_stream = train_stream.map(convert_to_generation_record)
         if val_stream:
-            error.value_check(
-                "<NLP63201425E>", len(val_stream) > 0, "val_stream cannot be empty"
-            )
-
             val_stream = val_stream.map(convert_to_generation_record)
 
         log.debug("Peft config [%s]", peft_config)
