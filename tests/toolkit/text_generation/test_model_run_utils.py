580078972430b401b73a910e91e4617fab8aab42
Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
2023-11-02 17:19:10 -0600

    black formatter
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>

--- tests/toolkit/text_generation/test_model_run_utils.py
+++ tests/toolkit/text_generation/test_model_run_utils.py
@@ -1,19 +1,16 @@
-# Standard
-from transformers import T5ForConditionalGeneration, AutoTokenizer
+# Third Party
+from transformers import AutoTokenizer, T5ForConditionalGeneration
 
 # First Party
-from caikit.interfaces.nlp.data_model import (
-    GeneratedTextResult
-)
+from caikit.interfaces.nlp.data_model import GeneratedTextResult
 
 # Local
-from tests.fixtures import (
-    SEQ2SEQ_LM_MODEL
-)
 from caikit_nlp.toolkit.text_generation.model_run_utils import generate_text_func
+from tests.fixtures import SEQ2SEQ_LM_MODEL
 
 model = T5ForConditionalGeneration.from_pretrained(SEQ2SEQ_LM_MODEL)
 tokenizer = AutoTokenizer.from_pretrained(SEQ2SEQ_LM_MODEL, model_max_length=512)
+
 
 def test_generate_text():
     train_kwargs = {
@@ -21,7 +18,7 @@ def test_generate_text():
         "tokenizer": tokenizer,
         "producer_id": 0,
         "eos_token": "<\n>",
-        "text": "I work at IBM"
+        "text": "I work at IBM",
     }
 
     pred = generate_text_func(**train_kwargs)
