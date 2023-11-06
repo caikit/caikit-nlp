a183208e5a2d11958ad9434a2e4287661eca3746
gkumbhat <kumbhat.gaurav@gmail.com>
2023-11-06 17:51:04 -0600

    :art: Fix linter
    Signed-off-by: gkumbhat <kumbhat.gaurav@gmail.com>

--- caikit_nlp/modules/text_generation/peft_prompt_tuning.py
+++ caikit_nlp/modules/text_generation/peft_prompt_tuning.py
@@ -41,7 +41,6 @@ import torch
 import transformers
 
 # First Party
-from caikit import get_config
 from caikit.core.data_model import DataStream
 from caikit.core.exceptions import error_handler
 from caikit.core.modules import ModuleBase, ModuleConfig, ModuleSaver, module
