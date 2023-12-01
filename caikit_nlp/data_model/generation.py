4eeb9348b1ade8974882d32e41942bfc1a8e158b
Sukriti-Sharma4 <sukriti.sharma4@ibm.com>
2023-11-30 17:12:57 -0700

    update LoraDataModel
    Signed-off-by: Sukriti-Sharma4 <sukriti.sharma4@ibm.com>

--- caikit_nlp/data_model/generation.py
+++ caikit_nlp/data_model/generation.py
@@ -77,34 +77,19 @@ class TuningConfig(DataObjectBase):
 class LoraTuningConfig(DataObjectBase):
     # Lora attention dimension.
     r: int
-    # The names of the modules to apply Lora to.
+    # List of module names or regex expression of the module names to replace with Lora.
+    # For example, ['q', 'v'] or '.*decoder.*(SelfAttention|EncDecAttention).*(q|v)$
     target_modules: Union[List[str], str]
     # The alpha parameter for Lora scaling.
     lora_alpha: int
     # The dropout probability for Lora layers.
     lora_dropout: float
-    # Set this to True if the layer to replace stores weight like (fan_in, fan_out).
-    # For example, gpt-2 uses Conv1D which stores weights like (fan_in, fan_out)
-    # and hence this should be set to True.
-    fan_in_fan_out: bool
     # Bias type for Lora. Can be ‘none’, ‘all’ or ‘lora_only’.
     # If ‘all’ or ‘lora_only’, the corresponding biases will be updated during training.
     # Be aware that this means that, even when disabling the adapters,
     # the model will not produce the same output
     # as the base model would have without adaptation.
     bias: str
-    # List of modules apart from LoRA layers to be set as trainable
-    # and saved in the final checkpoint.
-    modules_to_save: List[str]
-    # The layer indexes to transform, if this argument is specified,
-    # it will apply the LoRA transformations
-    # on the layer indexes that are specified in this list.
-    # If a single integer is passed,
-    # it will apply the LoRA transformations on the layer at this index.
-    layers_to_transform: Union[List[int], int]
-    # The layer pattern name, used only if layers_to_transform is different from None and
-    # if the layer pattern is not in the common layers pattern.
-    layers_pattern: str
     output_model_types: List[str]
 
 
