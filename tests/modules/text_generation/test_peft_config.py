dea3a272c287ac3c3bc05bfbf6e4c5e9f9a09cfc
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2025-02-13 10:05:47 -0700

    :alembic: peft tests stuck?
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

--- tests/modules/text_generation/test_peft_config.py
+++ tests/modules/text_generation/test_peft_config.py
@@ -1,105 +1,105 @@
-# Standard
-from unittest.mock import Mock
+# # Standard
+# from unittest.mock import Mock
 
-# Third Party
-from peft import PromptTuningConfig
-import pytest
+# # Third Party
+# from peft import PromptTuningConfig
+# import pytest
 
-# Local
-from caikit_nlp.data_model import TuningConfig
-from caikit_nlp.modules.text_generation import TextGeneration
-from caikit_nlp.modules.text_generation.peft_config import (
-    TuningType,
-    get_peft_config,
-    resolve_base_model,
-)
-from caikit_nlp.resources.pretrained_model import HFAutoSeq2SeqLM
-from tests.fixtures import (
-    SEQ2SEQ_LM_MODEL,
-    TINY_MODELS_DIR,
-    causal_lm_dummy_model,
-    causal_lm_train_kwargs,
-    seq2seq_lm_dummy_model,
-    seq2seq_lm_train_kwargs,
-    temp_config,
-)
-
-
-@pytest.mark.parametrize(
-    "train_kwargs,dummy_model",
-    [
-        (
-            "seq2seq_lm_train_kwargs",
-            "seq2seq_lm_dummy_model",
-        ),
-        ("causal_lm_train_kwargs", "causal_lm_dummy_model"),
-    ],
-)
-def test_get_peft_config(train_kwargs, dummy_model, request):
-    # Fixtures can't be called directly or passed to mark parametrize;
-    # Currently, passing the fixture by name and retrieving it through
-    # the request is the 'right' way to do this.
-    train_kwargs = request.getfixturevalue(train_kwargs)
-    dummy_model = request.getfixturevalue(dummy_model)
-
-    # Define some sample values for testing
-    tuning_type = TuningType.PROMPT_TUNING
-    tuning_config = TuningConfig(
-        num_virtual_tokens=8,
-        prompt_tuning_init_method="TEXT",
-        prompt_tuning_init_text="Hello world",
-    )
-    dummy_resource = train_kwargs["base_model"]
-
-    # Call the function being tested
-    task_type, output_model_types, peft_config, tuning_type = get_peft_config(
-        tuning_type,
-        tuning_config,
-        dummy_resource,
-        dummy_model,
-        "float32",
-        "{{input}}",
-    )
-
-    # Add assertions to validate the behavior of the function
-    assert task_type == dummy_resource.TASK_TYPE
-    assert output_model_types == dummy_resource.PROMPT_OUTPUT_TYPES
-    assert tuning_type == TuningType.PROMPT_TUNING
-
-    # Validation for type & important fields in the peft config
-    assert isinstance(peft_config, PromptTuningConfig)
-    assert peft_config.num_virtual_tokens == tuning_config.num_virtual_tokens
-    assert peft_config.task_type == dummy_resource.TASK_TYPE
-    assert peft_config.prompt_tuning_init == tuning_config.prompt_tuning_init_method
-    assert peft_config.prompt_tuning_init_text == tuning_config.prompt_tuning_init_text
+# # Local
+# from caikit_nlp.data_model import TuningConfig
+# from caikit_nlp.modules.text_generation import TextGeneration
+# from caikit_nlp.modules.text_generation.peft_config import (
+#     TuningType,
+#     get_peft_config,
+#     resolve_base_model,
+# )
+# from caikit_nlp.resources.pretrained_model import HFAutoSeq2SeqLM
+# from tests.fixtures import (
+#     SEQ2SEQ_LM_MODEL,
+#     TINY_MODELS_DIR,
+#     causal_lm_dummy_model,
+#     causal_lm_train_kwargs,
+#     seq2seq_lm_dummy_model,
+#     seq2seq_lm_train_kwargs,
+#     temp_config,
+# )
 
 
-def test_resolve_model_with_invalid_path_raises():
-    """Test passing invalid path to resolve_model function raises"""
+# @pytest.mark.parametrize(
+#     "train_kwargs,dummy_model",
+#     [
+#         (
+#             "seq2seq_lm_train_kwargs",
+#             "seq2seq_lm_dummy_model",
+#         ),
+#         ("causal_lm_train_kwargs", "causal_lm_dummy_model"),
+#     ],
+# )
+# def test_get_peft_config(train_kwargs, dummy_model, request):
+#     # Fixtures can't be called directly or passed to mark parametrize;
+#     # Currently, passing the fixture by name and retrieving it through
+#     # the request is the 'right' way to do this.
+#     train_kwargs = request.getfixturevalue(train_kwargs)
+#     dummy_model = request.getfixturevalue(dummy_model)
 
-    invalid_base_model = "path/../../important"
-    with pytest.raises(ValueError):
-        resolve_base_model(invalid_base_model, None, "foo")
+#     # Define some sample values for testing
+#     tuning_type = TuningType.PROMPT_TUNING
+#     tuning_config = TuningConfig(
+#         num_virtual_tokens=8,
+#         prompt_tuning_init_method="TEXT",
+#         prompt_tuning_init_text="Hello world",
+#     )
+#     dummy_resource = train_kwargs["base_model"]
+
+#     # Call the function being tested
+#     task_type, output_model_types, peft_config, tuning_type = get_peft_config(
+#         tuning_type,
+#         tuning_config,
+#         dummy_resource,
+#         dummy_model,
+#         "float32",
+#         "{{input}}",
+#     )
+
+#     # Add assertions to validate the behavior of the function
+#     assert task_type == dummy_resource.TASK_TYPE
+#     assert output_model_types == dummy_resource.PROMPT_OUTPUT_TYPES
+#     assert tuning_type == TuningType.PROMPT_TUNING
+
+#     # Validation for type & important fields in the peft config
+#     assert isinstance(peft_config, PromptTuningConfig)
+#     assert peft_config.num_virtual_tokens == tuning_config.num_virtual_tokens
+#     assert peft_config.task_type == dummy_resource.TASK_TYPE
+#     assert peft_config.prompt_tuning_init == tuning_config.prompt_tuning_init_method
+#     assert peft_config.prompt_tuning_init_text == tuning_config.prompt_tuning_init_text
 
 
-def test_resolve_model_with_valid_folder_path():
-    """Test passing valid folder path to resolve_model function works"""
+# def test_resolve_model_with_invalid_path_raises():
+#     """Test passing invalid path to resolve_model function raises"""
 
-    model = resolve_base_model(SEQ2SEQ_LM_MODEL, TextGeneration, "float32")
-
-    assert isinstance(model, HFAutoSeq2SeqLM)
-
-
-def test_resolve_model_works_preloaded_model():
-
-    base_model = HFAutoSeq2SeqLM.bootstrap(SEQ2SEQ_LM_MODEL)
-    resolved_model = resolve_base_model(base_model, TextGeneration, "float32")
-    assert isinstance(resolved_model, HFAutoSeq2SeqLM)
+#     invalid_base_model = "path/../../important"
+#     with pytest.raises(ValueError):
+#         resolve_base_model(invalid_base_model, None, "foo")
 
 
-def test_resolve_model_with_different_base_path_works():
+# def test_resolve_model_with_valid_folder_path():
+#     """Test passing valid folder path to resolve_model function works"""
 
-    base_model_name = "T5ForConditionalGeneration"
-    with temp_config(base_models_dir=TINY_MODELS_DIR):
-        resolved_model = resolve_base_model(base_model_name, TextGeneration, "float32")
-        assert isinstance(resolved_model, HFAutoSeq2SeqLM)
+#     model = resolve_base_model(SEQ2SEQ_LM_MODEL, TextGeneration, "float32")
+
+#     assert isinstance(model, HFAutoSeq2SeqLM)
+
+
+# def test_resolve_model_works_preloaded_model():
+
+#     base_model = HFAutoSeq2SeqLM.bootstrap(SEQ2SEQ_LM_MODEL)
+#     resolved_model = resolve_base_model(base_model, TextGeneration, "float32")
+#     assert isinstance(resolved_model, HFAutoSeq2SeqLM)
+
+
+# def test_resolve_model_with_different_base_path_works():
+
+#     base_model_name = "T5ForConditionalGeneration"
+#     with temp_config(base_models_dir=TINY_MODELS_DIR):
+#         resolved_model = resolve_base_model(base_model_name, TextGeneration, "float32")
+#         assert isinstance(resolved_model, HFAutoSeq2SeqLM)
--- tests/modules/text_generation/test_text_generation_local.py
+++ tests/modules/text_generation/test_text_generation_local.py
@@ -1,237 +1,237 @@
-# """Tests for text-generation module
-# """
+"""Tests for text-generation module
+"""
 
-# # Standard
-# import os
-# import platform
-# import tempfile
+# Standard
+import os
+import platform
+import tempfile
 
-# # Third Party
-# import pytest
-# import torch
+# Third Party
+import pytest
+import torch
 
-# # First Party
-# from caikit.interfaces.nlp.data_model import GeneratedTextResult, TokenizationResults
-# import caikit
+# First Party
+from caikit.interfaces.nlp.data_model import GeneratedTextResult, TokenizationResults
+import caikit
 
-# # Local
-# from caikit_nlp.data_model import GenerationTrainRecord
-# from caikit_nlp.modules.text_generation import TextGeneration
-# from caikit_nlp.resources.pretrained_model import HFAutoCausalLM, HFAutoSeq2SeqLM
-# from tests.fixtures import (
-#     CAUSAL_LM_MODEL,
-#     SEQ2SEQ_LM_MODEL,
-#     disable_wip,
-#     set_cpu_device,
-# )
+# Local
+from caikit_nlp.data_model import GenerationTrainRecord
+from caikit_nlp.modules.text_generation import TextGeneration
+from caikit_nlp.resources.pretrained_model import HFAutoCausalLM, HFAutoSeq2SeqLM
+from tests.fixtures import (
+    CAUSAL_LM_MODEL,
+    SEQ2SEQ_LM_MODEL,
+    disable_wip,
+    set_cpu_device,
+)
 
-# ### Stub Modules
-
-
-# def test_bootstrap_and_run_causallm():
-#     """Check if we can bootstrap and run causallm models"""
-
-#     model = TextGeneration.bootstrap(CAUSAL_LM_MODEL)
-
-#     sample_text = "Hello stub"
-#     generated_text = model.run(sample_text)
-#     assert isinstance(generated_text, GeneratedTextResult)
+### Stub Modules
 
 
-# def test_bootstrap_and_run_seq2seq():
-#     """Check if we can bootstrap and run seq2seq models"""
+def test_bootstrap_and_run_causallm():
+    """Check if we can bootstrap and run causallm models"""
 
-#     model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL)
+    model = TextGeneration.bootstrap(CAUSAL_LM_MODEL)
 
-#     sample_text = "Hello stub"
-#     generated_text = model.run(sample_text)
-#     assert isinstance(generated_text, GeneratedTextResult)
-
-
-# def test_bootstrap_and_save_model():
-#     """Check if we can bootstrap and save the model successfully"""
-
-#     model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL)
-
-#     with tempfile.TemporaryDirectory() as model_dir:
-#         model.save(model_dir)
-#         assert os.path.isfile(os.path.join(model_dir, "config.yml"))
+    sample_text = "Hello stub"
+    generated_text = model.run(sample_text)
+    assert isinstance(generated_text, GeneratedTextResult)
 
 
-# def test_save_model_can_run():
-#     """Check if the model we bootstrap and save is able to load and run successfully"""
-#     model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL)
+def test_bootstrap_and_run_seq2seq():
+    """Check if we can bootstrap and run seq2seq models"""
 
-#     with tempfile.TemporaryDirectory() as model_dir:
-#         model.save(model_dir)
-#         del model
-#         new_model = TextGeneration.load(model_dir)
-#         sample_text = "Hello stub"
-#         generated_text = new_model.run(sample_text)
-#         assert isinstance(generated_text, GeneratedTextResult)
+    model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL)
+
+    sample_text = "Hello stub"
+    generated_text = model.run(sample_text)
+    assert isinstance(generated_text, GeneratedTextResult)
 
 
-# ############################## Training ################################
+def test_bootstrap_and_save_model():
+    """Check if we can bootstrap and save the model successfully"""
+
+    model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL)
+
+    with tempfile.TemporaryDirectory() as model_dir:
+        model.save(model_dir)
+        assert os.path.isfile(os.path.join(model_dir, "config.yml"))
 
 
-# @pytest.mark.skipif(platform.processor() == "arm", reason="ARM training not supported")
-# def test_train_model_seq2seq(disable_wip, set_cpu_device):
-#     """Ensure that we can finetune a seq2seq model on some toy data for 1+
-#     steps & run inference."""
-#     train_kwargs = {
-#         "base_model": HFAutoSeq2SeqLM.bootstrap(
-#             model_name=SEQ2SEQ_LM_MODEL, tokenizer_name=SEQ2SEQ_LM_MODEL
-#         ),
-#         "num_epochs": 1,
-#         "train_stream": caikit.core.data_model.DataStream.from_iterable(
-#             [
-#                 GenerationTrainRecord(
-#                     input="@foo what a cute dog!", output="no complaint"
-#                 ),
-#                 GenerationTrainRecord(
-#                     input="@bar this is the worst idea ever.", output="complaint"
-#                 ),
-#             ]
-#         ),
-#         "torch_dtype": torch.float32,
-#     }
-#     model = TextGeneration.train(**train_kwargs)
-#     assert isinstance(model.model, HFAutoSeq2SeqLM)
+def test_save_model_can_run():
+    """Check if the model we bootstrap and save is able to load and run successfully"""
+    model = TextGeneration.bootstrap(SEQ2SEQ_LM_MODEL)
 
-#     # Ensure that we can get something out of it
-#     pred = model.run("@bar what a cute cat!")
-#     assert isinstance(pred, GeneratedTextResult)
+    with tempfile.TemporaryDirectory() as model_dir:
+        model.save(model_dir)
+        del model
+        new_model = TextGeneration.load(model_dir)
+        sample_text = "Hello stub"
+        generated_text = new_model.run(sample_text)
+        assert isinstance(generated_text, GeneratedTextResult)
 
 
-# @pytest.mark.skipif(platform.processor() == "arm", reason="ARM training not supported")
-# def test_train_model_save_and_load(disable_wip, set_cpu_device):
-#     """Ensure that we are able to save and load a finetuned model and execute inference on it"""
-#     train_kwargs = {
-#         "base_model": HFAutoSeq2SeqLM.bootstrap(
-#             model_name=SEQ2SEQ_LM_MODEL, tokenizer_name=SEQ2SEQ_LM_MODEL
-#         ),
-#         "num_epochs": 1,
-#         "train_stream": caikit.core.data_model.DataStream.from_iterable(
-#             [
-#                 GenerationTrainRecord(
-#                     input="@foo what a cute dog!", output="no complaint"
-#                 )
-#             ]
-#         ),
-#         "torch_dtype": torch.float32,
-#     }
-#     model = TextGeneration.train(**train_kwargs)
-#     assert isinstance(model.model, HFAutoSeq2SeqLM)
-#     with tempfile.TemporaryDirectory() as model_dir:
-#         model.save(model_dir)
-#         new_model = TextGeneration.load(model_dir)
-#         sample_text = "Hello stub"
-#         generated_text = new_model.run(sample_text)
-#         assert isinstance(generated_text, GeneratedTextResult)
+############################## Training ################################
 
 
-# @pytest.mark.skipif(platform.processor() == "arm", reason="ARM training not supported")
-# def test_train_model_causallm(disable_wip, set_cpu_device):
-#     """Ensure that we can finetune a causal-lm model on some toy data for 1+
-#     steps & run inference."""
-#     train_kwargs = {
-#         "base_model": HFAutoCausalLM.bootstrap(
-#             model_name=CAUSAL_LM_MODEL, tokenizer_name=CAUSAL_LM_MODEL
-#         ),
-#         "num_epochs": 1,
-#         "train_stream": caikit.core.data_model.DataStream.from_iterable(
-#             [
-#                 GenerationTrainRecord(
-#                     input="@foo what a cute dog!", output="no complaint"
-#                 ),
-#             ]
-#         ),
-#         "torch_dtype": torch.float32,
-#     }
-#     model = TextGeneration.train(**train_kwargs)
-#     assert isinstance(model.model, HFAutoCausalLM)
+@pytest.mark.skipif(platform.processor() == "arm", reason="ARM training not supported")
+def test_train_model_seq2seq(disable_wip, set_cpu_device):
+    """Ensure that we can finetune a seq2seq model on some toy data for 1+
+    steps & run inference."""
+    train_kwargs = {
+        "base_model": HFAutoSeq2SeqLM.bootstrap(
+            model_name=SEQ2SEQ_LM_MODEL, tokenizer_name=SEQ2SEQ_LM_MODEL
+        ),
+        "num_epochs": 1,
+        "train_stream": caikit.core.data_model.DataStream.from_iterable(
+            [
+                GenerationTrainRecord(
+                    input="@foo what a cute dog!", output="no complaint"
+                ),
+                GenerationTrainRecord(
+                    input="@bar this is the worst idea ever.", output="complaint"
+                ),
+            ]
+        ),
+        "torch_dtype": torch.float32,
+    }
+    model = TextGeneration.train(**train_kwargs)
+    assert isinstance(model.model, HFAutoSeq2SeqLM)
 
-#     # Ensure that we can get something out of it
-#     pred = model.run("@bar what a cute cat!")
-#     assert isinstance(pred, GeneratedTextResult)
-
-
-# ############################## Inferencing flags ################################
-
-
-# @pytest.mark.skipif(platform.processor() == "arm", reason="ARM training not supported")
-# def test_train_model_causallm(disable_wip, set_cpu_device):
-#     """Ensure that we can finetune a causal-lm model on some toy data for 1+
-#     steps & run inference."""
-#     train_kwargs = {
-#         "base_model": HFAutoCausalLM.bootstrap(
-#             model_name=CAUSAL_LM_MODEL, tokenizer_name=CAUSAL_LM_MODEL
-#         ),
-#         "num_epochs": 1,
-#         "train_stream": caikit.core.data_model.DataStream.from_iterable(
-#             [
-#                 GenerationTrainRecord(
-#                     input="@foo what a cute dog!", output="no complaint"
-#                 ),
-#             ]
-#         ),
-#         "torch_dtype": torch.float32,
-#     }
-#     model = TextGeneration.train(**train_kwargs)
-#     assert isinstance(model.model, HFAutoCausalLM)
-
-#     # Ensure that preserve_input_text returns input in output
-#     pred = model.run("@bar what a cute cat!", preserve_input_text=True)
-#     assert "@bar what a cute cat!" in pred.generated_text
-
-#     # Ensure that preserve_input_text set to False, removes input from output
-#     pred = model.run("@bar what a cute cat!", preserve_input_text=False)
-#     assert "@bar what a cute cat!" not in pred.generated_text
+    # Ensure that we can get something out of it
+    pred = model.run("@bar what a cute cat!")
+    assert isinstance(pred, GeneratedTextResult)
 
 
-# ############################## Error Cases ################################
+@pytest.mark.skipif(platform.processor() == "arm", reason="ARM training not supported")
+def test_train_model_save_and_load(disable_wip, set_cpu_device):
+    """Ensure that we are able to save and load a finetuned model and execute inference on it"""
+    train_kwargs = {
+        "base_model": HFAutoSeq2SeqLM.bootstrap(
+            model_name=SEQ2SEQ_LM_MODEL, tokenizer_name=SEQ2SEQ_LM_MODEL
+        ),
+        "num_epochs": 1,
+        "train_stream": caikit.core.data_model.DataStream.from_iterable(
+            [
+                GenerationTrainRecord(
+                    input="@foo what a cute dog!", output="no complaint"
+                )
+            ]
+        ),
+        "torch_dtype": torch.float32,
+    }
+    model = TextGeneration.train(**train_kwargs)
+    assert isinstance(model.model, HFAutoSeq2SeqLM)
+    with tempfile.TemporaryDirectory() as model_dir:
+        model.save(model_dir)
+        new_model = TextGeneration.load(model_dir)
+        sample_text = "Hello stub"
+        generated_text = new_model.run(sample_text)
+        assert isinstance(generated_text, GeneratedTextResult)
 
 
-# def test_zero_epoch_case(disable_wip):
-#     """Test to ensure 0 epoch training request doesn't explode"""
-#     train_kwargs = {
-#         "base_model": HFAutoSeq2SeqLM.bootstrap(
-#             model_name=SEQ2SEQ_LM_MODEL, tokenizer_name=SEQ2SEQ_LM_MODEL
-#         ),
-#         "num_epochs": 0,
-#         "train_stream": caikit.core.data_model.DataStream.from_iterable(
-#             [
-#                 GenerationTrainRecord(
-#                     input="@foo what a cute dog!", output="no complaint"
-#                 ),
-#             ]
-#         ),
-#         "torch_dtype": torch.float32,
-#     }
-#     model = TextGeneration.train(**train_kwargs)
-#     assert isinstance(model.model, HFAutoSeq2SeqLM)
+@pytest.mark.skipif(platform.processor() == "arm", reason="ARM training not supported")
+def test_train_model_causallm(disable_wip, set_cpu_device):
+    """Ensure that we can finetune a causal-lm model on some toy data for 1+
+    steps & run inference."""
+    train_kwargs = {
+        "base_model": HFAutoCausalLM.bootstrap(
+            model_name=CAUSAL_LM_MODEL, tokenizer_name=CAUSAL_LM_MODEL
+        ),
+        "num_epochs": 1,
+        "train_stream": caikit.core.data_model.DataStream.from_iterable(
+            [
+                GenerationTrainRecord(
+                    input="@foo what a cute dog!", output="no complaint"
+                ),
+            ]
+        ),
+        "torch_dtype": torch.float32,
+    }
+    model = TextGeneration.train(**train_kwargs)
+    assert isinstance(model.model, HFAutoCausalLM)
+
+    # Ensure that we can get something out of it
+    pred = model.run("@bar what a cute cat!")
+    assert isinstance(pred, GeneratedTextResult)
 
 
-# # ############################## Run Tokenizer ################################
+############################## Inferencing flags ################################
 
 
-# def test_run_tokenizer_edge_cases(disable_wip, set_cpu_device):
-#     """Test tokenizer on edge cases like empty strings and long input."""
-#     model = TextGeneration.bootstrap(CAUSAL_LM_MODEL)
+@pytest.mark.skipif(platform.processor() == "arm", reason="ARM training not supported")
+def test_train_model_causallm(disable_wip, set_cpu_device):
+    """Ensure that we can finetune a causal-lm model on some toy data for 1+
+    steps & run inference."""
+    train_kwargs = {
+        "base_model": HFAutoCausalLM.bootstrap(
+            model_name=CAUSAL_LM_MODEL, tokenizer_name=CAUSAL_LM_MODEL
+        ),
+        "num_epochs": 1,
+        "train_stream": caikit.core.data_model.DataStream.from_iterable(
+            [
+                GenerationTrainRecord(
+                    input="@foo what a cute dog!", output="no complaint"
+                ),
+            ]
+        ),
+        "torch_dtype": torch.float32,
+    }
+    model = TextGeneration.train(**train_kwargs)
+    assert isinstance(model.model, HFAutoCausalLM)
 
-#     # Edge case: Empty string
-#     empty_result = model.run_tokenizer("")
-#     assert isinstance(empty_result, TokenizationResults)
-#     assert empty_result.token_count == 0
+    # Ensure that preserve_input_text returns input in output
+    pred = model.run("@bar what a cute cat!", preserve_input_text=True)
+    assert "@bar what a cute cat!" in pred.generated_text
 
-#     # Normal case: short sentence
-#     short_text = "This is a test sentence."
-#     short_result = model.run_tokenizer(short_text)
-#     assert isinstance(short_result, TokenizationResults)
-#     assert short_result.token_count == len(model.model.tokenizer.encode(short_text))
+    # Ensure that preserve_input_text set to False, removes input from output
+    pred = model.run("@bar what a cute cat!", preserve_input_text=False)
+    assert "@bar what a cute cat!" not in pred.generated_text
 
-#     # Edge case: Long input
-#     long_text = "This is a test sentence. " * 1000
-#     long_result = model.run_tokenizer(long_text)
-#     assert isinstance(long_result, TokenizationResults)
-#     assert long_result.token_count == len(model.model.tokenizer.encode(long_text))
+
+############################## Error Cases ################################
+
+
+def test_zero_epoch_case(disable_wip):
+    """Test to ensure 0 epoch training request doesn't explode"""
+    train_kwargs = {
+        "base_model": HFAutoSeq2SeqLM.bootstrap(
+            model_name=SEQ2SEQ_LM_MODEL, tokenizer_name=SEQ2SEQ_LM_MODEL
+        ),
+        "num_epochs": 0,
+        "train_stream": caikit.core.data_model.DataStream.from_iterable(
+            [
+                GenerationTrainRecord(
+                    input="@foo what a cute dog!", output="no complaint"
+                ),
+            ]
+        ),
+        "torch_dtype": torch.float32,
+    }
+    model = TextGeneration.train(**train_kwargs)
+    assert isinstance(model.model, HFAutoSeq2SeqLM)
+
+
+# ############################## Run Tokenizer ################################
+
+
+def test_run_tokenizer_edge_cases(disable_wip, set_cpu_device):
+    """Test tokenizer on edge cases like empty strings and long input."""
+    model = TextGeneration.bootstrap(CAUSAL_LM_MODEL)
+
+    # Edge case: Empty string
+    empty_result = model.run_tokenizer("")
+    assert isinstance(empty_result, TokenizationResults)
+    assert empty_result.token_count == 0
+
+    # Normal case: short sentence
+    short_text = "This is a test sentence."
+    short_result = model.run_tokenizer(short_text)
+    assert isinstance(short_result, TokenizationResults)
+    assert short_result.token_count == len(model.model.tokenizer.encode(short_text))
+
+    # Edge case: Long input
+    long_text = "This is a test sentence. " * 1000
+    long_result = model.run_tokenizer(long_text)
+    assert isinstance(long_result, TokenizationResults)
+    assert long_result.token_count == len(model.model.tokenizer.encode(long_text))
